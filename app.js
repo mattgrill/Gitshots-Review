82922fee0147ee0c9507477350fd640983afbe63
Matthew Grill <matt@fourkitchens.com>
2014-08-23 13:53:47 -0700

    Pool queries.

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var express         = require('express'),
     bodyParser      = require('body-parser'),
     path            = require('path'),
     mysql           = require('mysql'),
-    connection      = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+    pool            = mysql.createPool({
+      connectionLimit : 10,
+      host            : process.env.MYSQL_HOST,
+      user            : process.env.MYSQL_USER,
+      password        : process.env.MYSQL_PASSWORD,
+      database        : process.env.MYSQL_DATABASE
+    });
 
 app.use(bodyParser.json());
 
@@ -11,17 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-connection.connect();
-
 app.post('/', function(req, res){
-  var query = connection.query('INSERT INTO _img_values SET ?', req.body, function(err, result) {
+  var query = pool.query('INSERT INTO _img_values SET ?', req.body, function(err, result) {
     res.status(200).send('Submitted to MYSQL. \n');
   });
 });
 
 app.get('/', function(req, res){
   var rows  = [],
-      query = connection.query('SELECT file_name from _img_values');
+      query = pool.query('SELECT file_name from _img_values');
   query
     .on('result', function(row) {
       rows.push(row);
@@ -32,4 +36,4 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 5000);
